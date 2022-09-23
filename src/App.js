1caf07511dec07032fadc6c89e0c78279dd12f62
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2022-09-23 14:29:27 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ModalDeparmento from  '../src/Components/modalDepartamento'
+
+
+function App() {
+  return(
+    <ModalDeparmento/>
+  )
+}
+export default App;
+
